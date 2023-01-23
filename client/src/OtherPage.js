481842631f8c5a16b6e29879cd1e502d20472ced
Damian Flis <damianflis1999@gmail.com>
2023-01-23 14:46:26 +0100

    Initial commit

---
+++ client/src/OtherPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export default () => {
+  return (
+    <div>
+      MultiFib<br></br>
+      Damian Flis<br></br>
+      I2S 2.2<br></br>
+      <Link to="/">Go back home</Link>
+    </div>
+  );
+};
